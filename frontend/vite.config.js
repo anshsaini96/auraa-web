338127b679cd173083cdfb10c7a29a2e0a232cf6
Ansh Saini <free90405@gmail.com>
2025-07-14 23:57:09 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['react-router-dom']
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      // 👇 Tell Vite to ignore admin folder completely
+      external: ['admin']
+    }
   },
   server: {
     port: 5173
